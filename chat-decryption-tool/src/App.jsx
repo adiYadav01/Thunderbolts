16283dae16efb556f336c7016bb9803fad03aadf
batmanaditya <161623863+batmanaditya@users.noreply.github.com>
2025-01-17 18:09:24 +0530

    homepage is made

---
+++ chat-decryption-tool/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './components/Home'; // Import the Home component
+
+function App() {
+    return (
+        <div>
+            <Home /> {/* Render the Home component */}
+        </div>
+    );
+}
+
+export default App;
